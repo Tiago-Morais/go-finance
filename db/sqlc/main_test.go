2940dd20bf1fcd3ccd65534a14e48dfa76c69fa9
Tiago-Morais <tiagokiokushin@gmail.com>
2022-10-04 21:21:41 -0300

    teste

--- db/sqlc/main_test.go
+++ db/sqlc/main_test.go
@@ -18,7 +18,7 @@ var testQueries *Queries
 func TestMain(m *testing.M) {
 	conn, err := sql.Open(dbDriver,dbSource)
 	if err != nil{
-		log.Fatal("cannot to connect do db: ", err)
+		log.Fatal("cannot to connect do db:fff ", err)
 	}
 	testQueries = New(conn)
 	os.Exit(m.Run())
